add86953e2e0e3166ef74b4cbe044cac8b423d4f
pandeynakul <pandeynakul.java@gmail.com>
2026-08-31 12:06:06 +0530

    practice brush up 2026

--- src/com/aug2026/CheckWordVowels.java
+++ src/com/aug2026/CheckWordVowels.java
@@ -1,6 +1,16 @@
 package com.aug2026;
 
- /* author :  Ankul Deshpande */
- 
- public class CheckWordVowels {
+/* author :  Ankul Deshpande */
+
+import java.util.Arrays;
+
+public class CheckWordVowels {
+    public static void main(String[] args) {
+
+        String sentence = "i java Expert near about ten years of Experience";
+
+        Arrays.stream(sentence.split(" ")).
+                filter(x -> x.replaceAll("[^aeiouAEIOU]", "").length() == 2).
+                forEach(x -> System.out.print(x+" "));
+    }
 }
--- src/com/aug2026/EvenOdd.java
+++ src/com/aug2026/EvenOdd.java
@@ -1,6 +1,30 @@
 package com.aug2026;
 
- /* author :  Ankul Deshpande */
- 
- public class EvenOdd {
+/* author :  Ankul Deshpande */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //convert into the wrapper from integer
+        List<Integer> wrapped = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        List<List<Integer>> eo = wrapped.stream()
+                .collect(Collectors.groupingBy(x -> x % 2 == 0)).entrySet().stream().
+                map(x -> x.getValue()).collect(Collectors.toList());
+        eo.forEach(x -> System.out.println(x));
+        System.out.println("==================");
+        System.out.println("with the help of partition are also possible");
+        List<List<Integer>> eo1 = wrapped.stream()
+                .collect(Collectors.partitioningBy(x -> x % 2 == 0)).entrySet().stream().
+                map(x -> x.getValue()).collect(Collectors.toList());
+        eo.forEach(x -> System.out.println(x));
+
+
+    }
 }
